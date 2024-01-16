de03a597c81c9b35130d87f952a14689230e98bf
mahirmalik030 <148874446+mahirmalik030@users.noreply.github.com>
2024-01-16 22:49:10 +0500

    Add files via upload

---
+++ CALCULATOR.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b;
+	cout<<"ENTER A NUMBER:";
+	cin>>a;
+	cout<<"ENTER ANOTHER NUMBER:";
+	cin>>b;
+	cout<<"THE SUM IS FOLLOWING:"<<a+b<<endl;
+	cout<<"THE DEVISION IS FOLLOWING:"<<a/b<<endl;
+	cout<<"THE MUL IS FOLLOWING:"<<a*b<<endl;
+	cout<<"THE SUB IS FOLLOWING:"<<a-b<<endl;
+	cout<<"THANK YOU FOR USING MY CALCULATOR"<<endl;
+	
+	cout<<"THANK YOU FOR USING MY CALCULATOR";
+	system("pause");
+	return 0;
+}
